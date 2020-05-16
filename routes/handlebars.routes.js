8aa97ab1f165f4611c0ae2bb99b8819942240ac0
Diego Lehyt <diegolehy00@gmail.com>
2020-05-16 18:35:46 -0400

    First pages connected using Handlebars

--- routes/handlebars.routes.js
+++ routes/handlebars.routes.js
@@ -22,21 +22,22 @@ router.get('/landing', function (req, res) {
   res.render('landing')
 })
 
-// The route for viewing the blog reviews
-router.get('/new', function (req, res) {
-  res.render('new')
-})
-
 // The route for managing restaurants
 router.get('/restaurants', async function (req, res) {
   try {
     const restaurantsArray = await Restaurant.findAll()
+    console.log(restaurantsArray)
     res.status(200).render('restaurants', {restaurants: restaurantsArray})
   }
   catch (err) {
     res.status(500).json({ errors: [err] }) // change to better error display
   }
   
+})
+
+// The route for viewing the blog reviews
+router.get('/restaurants/new', function (req, res) {
+  res.render('new')
 })
 
 // The index route redirects to /landing route
